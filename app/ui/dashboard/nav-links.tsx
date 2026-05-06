1b63e81fa8e0fb05c52b4f9bcc3a70b751e27ac1
hirano-96 <mk.i-l-m.96@ezweb.ne.jp>
2026-05-07 01:19:41 +0900

    feat-ColorChange

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,14 +32,15 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex h-[48px] grow items-center justify-center rounded-md p-3 text-sm font-medium hover:bg-base-primary hover:text-base-foreground md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                "bg-sky-100 text-blue-600": pathname === link.href,
+                "bg-base-primary text-base-foreground border-r-4 border-accent-primary-dark":
+                  pathname === link.href,
               },
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden m-3 md:block">{link.name}</p>
           </Link>
         );
       })}
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,18 +6,18 @@ import { signOut } from "@/auth";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="bg-base-secondary flex h-full flex-col md:px-4">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="flex h-[60px] grow items-center justify-center rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
+        <div className="w-32 text-base-foreground md:w-40">
           <AcmeLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-row justify-between space-x-2 bg-base-secondary text-base-muted md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="hidden h-auto w-full grow rounded-md bg-base-secondary md:block"></div>
         <form
           action={async () => {
             "use server";

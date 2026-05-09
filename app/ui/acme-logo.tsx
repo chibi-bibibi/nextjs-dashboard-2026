9523a473605bcac0bfdcdc87756e78e58464dbcd
hirano-96 <mk.i-l-m.96@ezweb.ne.jp>
2026-05-09 18:33:32 +0900

    Fix-Css

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,7 +3,7 @@ import { lusitana } from "@/app/ui/fonts";
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-base-foreground`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-foreground`}
     >
       <p className="text-[28px]">Acme</p>
     </div>

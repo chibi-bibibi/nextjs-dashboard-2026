8f165aa29ec5632412327651b5fa3581803ed188
hirano-96 <mk.i-l-m.96@ezweb.ne.jp>
2026-05-09 17:31:46 +0900

    Fix-Css

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,12 @@
-import "@/app/ui/global.css";
 import SideNav from "@/app/ui/dashboard/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-base-primary flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="bg-primary flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="grow bg-base-primary p-6 md:overflow-y-auto md:p-12">
+      <div className="grow bg-primary p-6 md:overflow-y-auto md:p-12">
         {children}
       </div>
     </div>

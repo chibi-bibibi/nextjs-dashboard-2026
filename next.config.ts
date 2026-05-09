52d1d6d4f8d6d3d071ba0eaf5c9f6343575f640e
hirano-96 <mk.i-l-m.96@ezweb.ne.jp>
2026-05-09 14:34:29 +0900

    Fix-Css

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
